fe48bb65fad09ab654b1fcb0c8c48c2ce4f92d39
DohosMate <dohosmail@gmail.com>
2023-03-09 11:39:08 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Clock from './Components/Clock';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [timerDays, setTimerDays] = useState();
+  const [timerHours, setTimerHours] = useState();
+  const [timerMinutes, setTimerMinutes] = useState();
+  const [timerSeconds, setTimerSeconds] = useState();
+
+  let interval;
+
+  const startTimer = () =>{
+    const countDownDate = new Date("Dec 31,2023").getTime();
+
+    interval = setInterval(()=>{
+      const now = new Date().getTime();
+      const distance = countDownDate - now;
+      const days = Math.floor((distance / (24*60*60*1000)))
+      const hours = Math.floor((distance %(24*60*60*1000))/(1000*60*60))
+      const minutes = Math.floor((distance %(60*60*1000))/(1000*6))
+      const seconds = Math.floor((distance % (60*1000))/(1000))
+
+      if(distance<0){
+        //Stop timer
+        clearInterval(interval.current)
+      }else{
+        //Update timer
+        setTimerDays(days)
+        setTimerHours(hours)
+        setTimerMinutes(minutes)
+        setTimerSeconds(seconds)
+      }
+    })
+  }
+
+  useEffect(()=>{
+    startTimer();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock
+        timerDays={timerDays}
+        timerHours={timerHours}
+        timerMinutes={timerMinutes} 
+        timerSeconds={timerSeconds}
+      />
     </div>
   );
 }
